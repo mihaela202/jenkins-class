97f8e9804df478e21d2c95ea3bbe010c4ba6beca
mihaela202 <varanita.mihaela@gmail.com>
2020-09-08 18:46:36 -0500

    example job

---
+++ all-packages.groovy
@@ -0,0 +1,77 @@
+// Uniq name for the pod or slave 
+def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
+def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers:
+        - name: packer
+          image: hashicorp/packer:1.6.2
+          imagePullPolicy: IfNotPresent
+          command:
+          - cat
+          tty: true
+        - name: terraform
+          image: hashicorp/terraform:0.12.27
+          imagePullPolicy: IfNotPresent
+          command:
+          - cat
+          tty: true
+        - name: docker
+          image: docker:latest
+          imagePullPolicy: IfNotPresent
+          command:
+          - cat
+          tty: true
+          volumeMounts:
+            - mountPath: /var/run/docker.sock
+              name: docker-sock
+        serviceAccountName: default
+        securityContext:
+          runAsUser: 0
+          fsGroup: 0
+        volumes:
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
+    """
+
+properties([
+    [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], 
+    parameters([
+        booleanParam(defaultValue: false, description: 'Please select this to be able to run the job on debug mode. ', name: 'debugMode')
+        ])
+    ])    
+podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: params.debugMode ) {
+    node(k8slabel) {
+    stage("Docker check") {
+        container("docker") {
+            sh 'docker --version'
+        }
+    }
+    stage("Terraform Check") {
+        container("terraform") {
+            sh 'terraform version'
+        }
+    }
+    stage("Packer Check") {
+        container("packer") {
+            sh 'packer version'
+        }
+    }
+    }
+
+}
