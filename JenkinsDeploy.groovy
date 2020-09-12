a3ac6a78a7f0ad2f0ad1324d2520cd11fdd03792
mihaela202 <varanita.mihaela@gmail.com>
2020-09-12 16:16:13 -0500

    commit changes

--- JenkinsDeploy.groovy
+++ JenkinsDeploy.groovy
@@ -1,17 +1,15 @@
+// Uniq name for the pod or slave 
 def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
-
 properties([
     [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], 
     parameters([
-        booleanParam(defaultValue: false, description: 'Please select to apply all changes to environment ', name: 'applyChanges'), 
-        booleanParam(defaultValue: false, description: 'Please select to destroy all changes to environment ', name: 'destroyChanges'), 
-        string(defaultValue: '', description: 'Please provide the Docker image to deploy', name: 'selectedDockerImage', trim: true), 
-        choice(choices: ['dev ', 'qa', 'stage', 'prod'], description: 'Please provide the environment to deploy ', name: 'environment ')
+        booleanParam(defaultValue: false, description: 'Please select to apply all changes to the environment', name: 'applyChanges'), 
+        booleanParam(defaultValue: false, description: 'Please select to destroy all changes to the environment', name: 'destroyChanges'), 
+        string(defaultValue: '', description: 'Please provide the docker to deploy ', name: 'selectedDockerImage', trim: true), 
+        choice(choices: ['dev', 'qa', 'stage', 'prod'], description: 'Please provide the environment to deploy ', name: 'environment')
         ])
         ])
-
-// Uniq name for the pod or slave 
-
+// yaml def for slaves 
 def slavePodTemplate = """
       metadata:
         labels:
@@ -30,13 +28,12 @@ def slavePodTemplate = """
                   - jenkins-jenkins-master
               topologyKey: "kubernetes.io/hostname"
         containers:
-       - name: fuchicorptools
+        - name: fuchicorptools
           image: fuchicorp/buildtools
           imagePullPolicy: Always
           command:
           - cat
           tty: true
-        
         serviceAccountName: default
         securityContext:
           runAsUser: 0
@@ -48,9 +45,8 @@ def slavePodTemplate = """
     """
     podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
       node(k8slabel) {
-        stage("Pull the SCM") {            
+        stage("Pull the SCM") {
             git 'https://github.com/mihaela202/jenkins-class'
-            
         }
         stage("Apply/Plan") {
             container("fuchicorptools") {
@@ -59,15 +55,3 @@ def slavePodTemplate = """
         }
       }
     }
-
-
-println(
-    """
-    Apply changes: ${params.applyChanges}
-    Destroy changes ${params.destroyChanges}
-    Docker image ${params.selectedDockerImage}
-    Enviroment ${params.environment}
-
-
-    """
-)
