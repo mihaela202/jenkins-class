b238d11946c689cdae9904fd4090b75b97fb5c86
mihaela202 <varanita.mihaela@gmail.com>
2020-09-12 16:10:52 -0500

    added stages to deplpy job

--- JenkinsDeploy.groovy
+++ JenkinsDeploy.groovy
@@ -1,3 +1,5 @@
+def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
+
 properties([
     [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], 
     parameters([
@@ -7,6 +9,58 @@ properties([
         choice(choices: ['dev ', 'qa', 'stage', 'prod'], description: 'Please provide the environment to deploy ', name: 'environment ')
         ])
         ])
+
+// Uniq name for the pod or slave 
+
+def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers:
+       - name: fuchicorptools
+          image: fuchicorp/buildtools
+          imagePullPolicy: Always
+          command:
+          - cat
+          tty: true
+        
+        serviceAccountName: default
+        securityContext:
+          runAsUser: 0
+          fsGroup: 0
+        volumes:
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
+    """
+    podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
+      node(k8slabel) {
+        stage("Pull the SCM") {            
+            git 'https://github.com/mihaela202/jenkins-class'
+            
+        }
+        stage("Apply/Plan") {
+            container("fuchicorptools") {
+                sh 'kubectl version'
+            }
+        }
+      }
+    }
+
+
 println(
     """
     Apply changes: ${params.applyChanges}
